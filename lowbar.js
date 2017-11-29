e9e2f5800c6bf7fc66da2a42234946a8919dc556
arpithapatil <arpitha.patil@outlook.com>
2017-11-29 16:37:25 +0000

    builds and tests for _.values

--- lowbar.js
+++ lowbar.js
@@ -4,6 +4,15 @@ Object.values = require('object.values');
 _.identity = function (value) {
   return value;
 };
+
+_.values = function (list) {
+  if (typeof list !== 'object') return [];
+  if (Array.isArray(list)) {
+    return list;
+  } else {
+    return (Object.values(list));
+  }
+}; 
   
 
 
